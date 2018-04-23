f1ef149623824dc23f3a3e5c8ee390d7b5e58579
Enzof60 <enzolieghio@gmail.com>
2018-04-24 00:57:52 +0100

    Commented node class

--- src/graph/Node.java
+++ src/graph/Node.java
@@ -2,17 +2,27 @@ package graph;
 
 import java.util.Random;
 
+// Node class used to emulate a node on a graph 
+// and to store information about the node
 public class Node {
 
+	// Private instance variables one integer id and one boolean visited
+	// they are private to adhere to encapsulation
 	private int id;
 	private boolean visited;
 
+	// Node constructor variable called when a node object is created
+	// takes in an integer value which is used as the node id
 	public Node(int id) {
+		// Initialize the values
 		super();
 		this.id = id;
 		this.visited = false;
 	}
 
+	// Getter and setter methods
+	// Used to set and get the values from the instance variable
+	// Used for encapsulation
 	public void setID(int id) {
 		this.id = id;
 	}
@@ -29,6 +39,8 @@ public class Node {
 		return visited;
 	}
 
+	// getX and getY methods used to generate a
+	// random x and y value to plot the node
 	public int getX() {
 		Random gen = new Random();
 		return gen.nextInt(3);
@@ -39,6 +51,7 @@ public class Node {
 		return gen.nextInt(3);
 	}
 
+	// Overridden class equals method used to compare two nodes are equal
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -50,10 +63,16 @@ public class Node {
 		return _obj.id == id;
 	}
 
+	// Overridden hashcode method to ensure when search for an item it can be found
+	// because each objects gets a unique hashcode which is integer that is
+	// constant, ensures that objects can be compared
 	@Override
 	public int hashCode() {
 		return id;
 	}
+
+	// Overridden toString method use to provide the
+	// content of an node object in a string format.
 	@Override
 	public String toString() {
 		String s = "{\"id\": \"n" + getID() + "\", " + "\"label\": \"node" + getID() + "\", " + "\"x\": \"" + getX()
